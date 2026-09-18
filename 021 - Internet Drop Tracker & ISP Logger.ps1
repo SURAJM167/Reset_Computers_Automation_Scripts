f7398d343326cd40c7d52d8709e9db71f0d16b05
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 21:19:03 +0530

    Create 021 - Internet Drop Tracker & ISP Logger.ps1

---
+++ 021 - Internet Drop Tracker & ISP Logger.ps1
@@ -0,0 +1,14 @@
+﻿$logFile = "C:\Scripts\Internet_Drops.txt"
+$wasDown = $false
+
+while ($true) {
+    $ping = Test-Connection 8.8.8.8 -Count 1 -Quiet
+    if (-not $ping -and -not $wasDown) {
+        "$(Get-Date) - INTERNET DOWN" | Out-File -Append $logFile
+        $wasDown = $true
+    } elseif ($ping -and $wasDown) {
+        "$(Get-Date) - INTERNET RESTORED" | Out-File -Append $logFile
+        $wasDown = $false
+    }
+    Start-Sleep -Seconds 5
+}
