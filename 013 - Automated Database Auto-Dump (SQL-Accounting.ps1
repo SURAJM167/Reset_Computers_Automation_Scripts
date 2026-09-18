28d8bc1940dafef1357295b7b9a50ed8e4b0a418
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 20:05:12 +0530

    Create 013 - Automated Database Auto-Dump (SQL-Accounting.ps1

---
+++ 013 - Automated Database Auto-Dump (SQL-Accounting.ps1
@@ -0,0 +1,8 @@
+﻿# MySQL required in system path
+$dbUser = "root"
+$dbPass = "password"
+$dbName = "accounting_db"
+$backupPath = "D:\Backups\DB_Backup_$(Get-Date -Format 'yyyyMMdd_HHmm').sql"
+
+& mysqldump -u $dbUser -p$dbPass $dbName > $backupPath
+Write-Host "Database backup saved to $backupPath"
