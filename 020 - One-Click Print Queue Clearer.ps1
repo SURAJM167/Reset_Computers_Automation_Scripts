e67afcb15b6b303a08deb77737d5622d640d2b18
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 20:06:49 +0530

    Create 020 - One-Click Print Queue Clearer.ps1

---
+++ 020 - One-Click Print Queue Clearer.ps1
@@ -0,0 +1,5 @@
+﻿Stop-Service -Name Spooler -Force
+Write-Host "Spooler stopped. Clearing queue..."
+Remove-Item -Path "$env:windir\System32\spool\PRINTERS\*.*" -Force -Recurse -ErrorAction SilentlyContinue
+Start-Service -Name Spooler
+Write-Host "Print queue cleared and Spooler restarted!" -ForegroundColor Green
