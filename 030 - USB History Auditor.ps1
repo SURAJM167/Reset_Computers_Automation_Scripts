2701c263ccacfdccb6c3b3879ba6bfe1701d2f81
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 21:19:27 +0530

    RC

---
+++ 030 - USB History Auditor.ps1
@@ -0,0 +1,9 @@
+﻿$usbPath = "HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR"
+$usbDevices = Get-ChildItem -Path $usbPath | ForEach-Object {
+    $deviceName = $_.PSChildName
+    [PSCustomObject]@{
+        Device = $deviceName.Split('&')[1]
+        Details = $deviceName
+    }
+}
+$usbDevices | Out-GridView -Title "USB Connection History"
