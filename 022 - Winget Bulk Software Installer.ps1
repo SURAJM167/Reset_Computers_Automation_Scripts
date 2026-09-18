f8789e2177658001b22877fe78aeb69f7bbde762
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 21:19:36 +0530

    Create 022 - Winget Bulk Software Installer.ps1

---
+++ 022 - Winget Bulk Software Installer.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Installing standard AMC softwares..." -ForegroundColor Cyan
+$softwares = @("Google.Chrome", "AnyDeskSoftwareGmbH.AnyDesk", "VideoLAN.VLC")
+
+foreach ($app in $softwares) {
+    Write-Host "Installing $app..."
+    winget install --id $app --silent --accept-package-agreements --accept-source-agreements
+}
+Write-Host "All softwares installed successfully!" -ForegroundColor Green
