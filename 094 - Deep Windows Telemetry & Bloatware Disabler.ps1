4f8c685e2435028fac47cd3c5b1c59cbacfb427f
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 21:24:41 +0530

    Create 094 - Deep Windows Telemetry & Bloatware Disabler.ps1

---
+++ 094 - Deep Windows Telemetry & Bloatware Disabler.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Disabling Windows Telemetry & Data Collection..." -ForegroundColor Yellow
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+if (-not (Test-Path $regPath)) { New-Item -Path $regPath -Force | Out-Null }
+Set-ItemProperty -Path $regPath -Name "AllowTelemetry" -Value 0
+
+Stop-Service -Name "DiagTrack" -Force -ErrorAction SilentlyContinue
+Set-Service -Name "DiagTrack" -StartupType Disabled
+Write-Host "Telemetry Disabled. Privacy enhanced and system sped up!" -ForegroundColor Green
