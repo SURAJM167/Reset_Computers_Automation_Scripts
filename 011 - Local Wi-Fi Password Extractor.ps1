553aa195eaa88f6a6630d9c54e9f8e9d678c31cc
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 20:04:34 +0530

    rc

---
+++ 011 - Local Wi-Fi Password Extractor.ps1
@@ -0,0 +1,8 @@
+﻿$profiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$_.Matches.Groups[1].Value.Trim()}
+$results = @()
+
+foreach ($profile in $profiles) {
+    $key = (netsh wlan show profile name="$profile" key=clear) | Select-String "Key Content\W+\:(.+)$" | %{$_.Matches.Groups[1].Value.Trim()}
+    if ($key) { $results += [PSCustomObject]@{ SSID = $profile; Password = $key } }
+}
+$results | Format-Table -AutoSize
