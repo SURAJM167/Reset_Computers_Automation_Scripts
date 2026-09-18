b586c6e6cc2d4f52e0a1e02011197b27114288e8
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 20:03:32 +0530

    IT Automation

---
+++ 006 - USB Port Locker.ps1
@@ -0,0 +1,12 @@
+﻿# Save as USBLock.ps1 (Run as Administrator)
+# USB Disable karna ho toh value 4 karein, Enable karna ho toh 3 karein
+$value = 4 
+
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
+Set-ItemProperty -Path $regPath -Name "Start" -Value $value
+
+if ($value -eq 4) {
+    Write-Host "USB Mass Storage is now LOCKED." -ForegroundColor Red
+} else {
+    Write-Host "USB Mass Storage is now UNLOCKED." -ForegroundColor Green
+}
