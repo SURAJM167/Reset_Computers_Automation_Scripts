af055819625b95e2e3c19a1ed225ef41a9a2c344
Reset Computers <surajmahla0223@gmail.com>
2026-09-18 20:04:48 +0530

    RC

---
+++ 012 - Simple Port Scanner.ps1
@@ -0,0 +1,11 @@
+﻿$target = "192.168.1.100"
+$ports = @(80, 443, 554, 8000, 3389)
+
+foreach ($port in $ports) {
+    $connection = Test-NetConnection -ComputerName $target -Port $port -WarningAction SilentlyContinue
+    if ($connection.TcpTestSucceeded) {
+        Write-Host "Port $port is OPEN on $target" -ForegroundColor Green
+    } else {
+        Write-Host "Port $port is CLOSED on $target" -ForegroundColor Red
+    }
+}
